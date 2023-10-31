e1710691ad621b50eb05314e0ed48d733228a022
iqbalshaid <111365870+iqbalshaid@users.noreply.github.com>
2023-10-31 08:31:43 +0530

    Add files via upload

---
+++ E-commerce project/script.js
@@ -0,0 +1,33 @@
+
+// var mainimage = document.querySelector(".img");
+// var smallimg = document.getElementsByClassName("img-col");
+
+// smallimg[0].onclick = function(){
+//     mainimage.src = smallimg[0].src;
+
+// }
+// smallimg[1].onclick = function(){
+//     mainimage.src = smallimg[1].src;
+
+// }
+// smallimg[2].onclick = function(){
+//     mainimage.src = smallimg[2].src;
+
+// }
+// smallimg[3].onclick = function(){
+//     mainimage.src = smallimg[3].src;
+
+// }
+var mn = document.querySelector(".menu");
+ 
+console.log("how are you")
+mn.onclick = function(){
+
+//console.log("fine");
+
+
+    var nav = document.querySelector(".navbar");
+   nav.classList.toggle("show");
+   
+}
+
